258c6782bbef0e7d304b9c58af1d06dba7515f62
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2016-08-27 22:13:31 +0200

    added heroku link to allowed hosts

--- Atila16/settings.py
+++ Atila16/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^2osts2_^&-=p%gmdfmav$aq1q=4c99(*)$i9oo&9om-m3#q9)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atila16-lama.herokuapp.com']
 
 
 # Application definition
