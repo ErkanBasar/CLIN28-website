507e8b742e99a8e998e27946ec76bec22f956ce2
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2016-09-04 05:16:14 +0200

    thank you alert added for after registration

--- main/views.py
+++ main/views.py
@@ -28,16 +28,15 @@ class Home(View):
 		underconst = False
 
 		if(underconst is False):
-			return render(request, 'video-background.html', {})
+			return render(request, 'video-background.html', {'registeralert':'False',})
 		else:
-			return render(request, 'under_construction.html', {})
+			return render(request, 'under_construction.html', {'registeralert':'False',})
 
 
 	def post(self, request):
 
 
 		if "register" in request.POST:				
-
 
 			name = request.POST['name']
 			email = request.POST['email']
@@ -53,13 +52,13 @@ class Home(View):
 			info = {'name':name,'email':email,'affiliation':affiliation, 'diet':diet,'hotel':hotel,'room':room}
 
 			for e in events:
-				info[e] = 'True'
-
-			print(info)
+				info[e] = 'Yes'
 
 			coll.insert(info)
 
-			return render(request, 'video-background.html', {})
+			return render(request, 'video-background.html', {
+				'registeralert':'True',
+			})
 
 
 
