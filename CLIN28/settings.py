9fd5abd250f573b0971a14ac5b5f543b8229cfc2
Chara Tsoukala <x.tsoukala@gmail.com>
2017-11-02 23:16:46 +0100

    added meta keywords and description

--- CLIN28/settings.py
+++ CLIN28/settings.py
@@ -1,6 +1,5 @@
 """
 Django settings for CLIN28 project.
-
 """
 
 import os
@@ -12,7 +11,6 @@ config.read("data/auth.ini")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 HOSTNAME = os.uname()[1]
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -21,13 +19,11 @@ SECRET_KEY = config.get('settings', 'secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 if HOSTNAME[:9] == "applejack":
-	DEBUG = False
+    DEBUG = False
 else:
-	DEBUG = True
+    DEBUG = True
 
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'clin28.cls.ru.nl','soothsayer.cls.ru.nl']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'clin28.cls.ru.nl', 'soothsayer.cls.ru.nl']
 
 # Application definition
 
@@ -37,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -46,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -71,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CLIN28.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -81,7 +76,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -125,18 +119,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-
-
-if HOSTNAME[:9] == "applejack":     #for the server side
+if HOSTNAME[:9] == "applejack":  # for the server side
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static")
     ]
-else:                               #to work on local
+else:  # to work on local
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = [
--- main/views.py
+++ main/views.py
@@ -1,100 +1,93 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.core.mail import send_mail
-from django.http import HttpResponseRedirect, HttpResponse
-
-from django.conf import settings
-
-import os
-import json
 import logging
 import pymongo as pm
 import configparser
-from datetime import datetime
+import os
 
-import program as p
 
 logging.basicConfig(
-        format='%(asctime)s, %(levelname)s: %(message)s',
-        filename='data/clin28.log',
-        datefmt='%d-%m-%Y, %H:%M',
-        level=logging.INFO)
+    format='%(asctime)s, %(levelname)s: %(message)s',
+    filename='data/clin28.log',
+    datefmt='%d-%m-%Y, %H:%M',
+    level=logging.INFO)
 
 c = configparser.ConfigParser()
 c.read("data/auth.ini")
 
-client = pm.MongoClient(c.get('db','host'), int(c.get('db','port')))
-clindb = client[c.get('db','db')]
+client = pm.MongoClient(c.get('db', 'host'), int(c.get('db', 'port')))
+clindb = client[c.get('db', 'db')]
 
-if os.uname()[1][:9] != "applejack":
-    clindb.authenticate(c.get('db','user'), c.get('db','pass'))
+authorized_hosts = ["applejack", "xarou.hom"]
+if os.uname()[1][:9] not in authorized_hosts:
+    clindb.authenticate(c.get('db', 'user'), c.get('db', 'pass'))
 
-program_collection = clindb[c.get('db','program_collection')]
-participants_collection = clindb[c.get('db','participants_collection')]
-organization_collection = clindb[c.get('db','organization_collection')]
-dates_collection = clindb[c.get('db','dates_collection')]
+program_collection = clindb[c.get('db', 'program_collection')]
+participants_collection = clindb[c.get('db', 'participants_collection')]
+organization_collection = clindb[c.get('db', 'organization_collection')]
+dates_collection = clindb[c.get('db', 'dates_collection')]
+
+organization_team = []
+if list(organization_collection.find()):
+    organization_team = list(organization_collection.find())[0]['team']
+
 
 class Home(View):
-
     def get(self, request):
+        #        Uncomment to display under construction page.
+        #        if(os.uname()[1][:9] == 'applejack'):
+        #            return render(request, 'under_construction.html', {})
 
-#        Uncomment to display under construction page.
-#        if(os.uname()[1][:9] == 'applejack'):
-#            return render(request, 'under_construction.html', {})
-#        else:
+        program = sorted(list(program_collection.find()), key=lambda k: k['time'])
+        for session in program:
+            if 'slots' in session:
+                session['slots'] = sorted(session['slots'], key=lambda k: k['time'])
 
-            program = sorted(list(program_collection.find()), key=lambda k: k['time'])
-            for session in program:
-                if('slots' in session):
-                    session['slots'] = sorted(session['slots'], key=lambda k: k['time'])
+        return render(request, 'base.html', {
+            'title': 'Computational Linguistics in the Netherlands',
+            'keys': 'CLIN28, CLIN 28, Computational Linguistics in the Netherlands, Nijmegen',
+            'program': program,
+            'organization_team': organization_team,
+        })
 
-            organization_team = list(organization_collection.find())[0]['team']
-
-            return render(request, 'base.html', {
-                'program': program,
-                'organization_team': organization_team,
-            })
 
 class Dates(View):
-
     def get(self, request):
-
-        organization_team = list(organization_collection.find())[0]['team']
-        dates_list = list(dates_collection.find().sort('order',1))
+        dates_list = list(dates_collection.find().sort('order', 1))
 
         return render(request, 'dates.html', {
+            'title': 'Important Dates',
+            'keys': 'CLIN28, CLIN 28, Important dates',
             'organization_team': organization_team,
             'dates_list': dates_list,
         })
 
+
 class Calls(View):
-
     def get(self, request):
-
-        organization_team = list(organization_collection.find())[0]['team']
-
         return render(request, 'calls.html', {
+            'title': 'Calls',
+            'keys': 'CLIN28, CLIN 28, calls, abstracts',
             'organization_team': organization_team,
         })
 
+
 class SharedTask(View):
-
     def get(self, request):
-
-        organization_team = list(organization_collection.find())[0]['team']
-        dates = dates_collection.find({'title' : 'Shared task: Spelling correction'})[0]
+        dates = dates_collection.find({'title': 'Shared task: Spelling correction'})[0]
 
         return render(request, 'shared_task.html', {
+            'title': 'Shared task',
+            'keys': 'CLIN28, CLIN 28, shared task, shared task call',
             'organization_team': organization_team,
             'dates': dates,
         })
 
+
 class ThesisPrize(View):
-
     def get(self, request):
-
-        organization_team = list(organization_collection.find())[0]['team']
-
         return render(request, 'stil_thesis_prize.html', {
+            'title': 'Thesis prize',
+            'keys': 'CLIN28, CLIN 28, thesis prize, best thesis',
             'organization_team': organization_team,
         })
